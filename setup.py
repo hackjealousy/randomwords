9813ba7ff7134043814eb9c29c7da2c7115283f7
Hack Jealousy <hackjealousy@thre.at>
2021-12-17 14:04:47 +0000

    creating structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'randomwords',
+        version = '0.0.1',
+        description = 'Generate Random Words',
+        author = 'hackjealousy',
+        author_email = 'hackjealousy@thre.at',
+        url = 'https://github.com/hackjealousy/randomwords',
+        scripts = ['scripts/randomwords.py']
+    )
